037c91927bf8e92b5e042c40ba11532e335c9ebf
Marnoux Manser <marnouxmanser@gmail.com>
2020-01-24 13:44:55 +0200

    fixed footer

---
+++ Migrations/202001240918239_movieDateAddedDefault.cs
@@ -0,0 +1,29 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class movieDateAddedDefault : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"declare @tableName varchar(128)
+        declare @columnName varchar(128)
+        declare @constraintName varchar(128)
+        
+        set @tableName = ''Movies''
+        set @columnName = ''DateAdded''
+
+        set @constraintName = (select top 1 constraint_name
+                               from information_schema.constraint_column_usage
+                               where table_name = @tableName and column_name = @columnName)
+        
+        exec (''alter table '' + @tableName + '' drop constraint '' + @constraintName + '''') 
+        alter table Movies add constraint DateAddedDefault default getdate() for DateAdded ");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Migrations/202001241015366_newConstraintOnMovie.cs
@@ -0,0 +1,31 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newConstraintOnMovie : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"declare @tableName varchar(128) = 'Movies'
+                  declare @columnName varchar(128) = 'DateAdded'
+                  declare @constraintName varchar(128)
+                  
+                  set @constraintName = (select top 1 constraint_name
+                                         from information_schema.constraint_column_usage
+                                         where table_name = @tableName and column_name = @columnName)
+                  
+                  exec ('alter table  ''' + @tableName + '''  drop constraint  ''' + @constraintName + '''')
+                  
+                  GO        
+                  
+                  alter table Movies 
+                  add constraint DateAddedDefault default getdate() 
+                  for DateAdded");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
